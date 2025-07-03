a24936a0f3eefa7a342f22ebb8e3bf946e2199df
yodan <yodanbrandon@gmail.com>
2025-07-03 16:45:15 +0200

    playerPrefab avec un vrai corps + animation de fin

---
+++ Assets/_/Features/TriggerFin/Runtime/GestionCam.cs
@@ -0,0 +1,22 @@
+using System;
+using Unity.Cinemachine;
+using UnityEngine;
+
+public class GestionCam : MonoBehaviour
+{
+    [SerializeField] private CinemachineCamera _cameraInGame;
+    [SerializeField] private CinemachineCamera _cameraDeFin;
+    [SerializeField] private GameObject _Player;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            _cameraInGame.Priority = 0;
+            _cameraDeFin.Priority = 50;
+            
+            _Player.GetComponent<Animator>().SetBool("IsWin", true);
+        }
+    }
+}
